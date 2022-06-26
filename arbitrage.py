ab6488417a259e506d6c45870fbe24d6a2c25122
Alexandre Tourre <alex.tourre@gmail.com>
2022-06-26 13:30:09 +0000

    Update arbitrage.py

--- arbitrage.py
+++ arbitrage.py
@@ -1,1 +1,13 @@
+#This model designs an operating strategy for a grid-connected battery 
+#with a given set of operational constraints, under the assumption of known future prices
+#https://the-turing-way.netlify.app/communication/binder/zero-to-binder.html
 
+#Load packages
+import pulp
+import os
+import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import time
+%matplotlib inline
